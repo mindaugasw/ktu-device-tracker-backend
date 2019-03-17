a4eea60e670478e38acd03b8c3915b0c56564d93
Mindaugas <mindaugas932@gmail.com>
2019-03-18 00:29:04 +0200

    Trying to fix not working doctrine

--- src/Entity/Device.php
+++ src/Entity/Device.php
@@ -9,19 +9,43 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Device
 {
+	const OS_ANDROID = 1;
+	const OS_IOS = 2;
+	
     /**
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
      */
     private $id;
-
+	
+	/**
+	 * @ORM\Column(type="string", length=255)
+	 */
     private $name;
+    
     //private $last user;
+	
+	/**
+	 * @ORM\Column(type="boolean")
+	 */
 	private $simCard;
+	
+	/**
+	 * @ORM\Column(type="smallint")
+	 */
 	private $os;
+	
+	/**
+	 * @ORM\Column(type="string", length=255)
+	 */
 	private $osVersion;
+	
+	/**
+	 * @ORM\Column(type="boolean")
+	 */
 	private $enabled;
+	
 	
     public function getId(): ?int
     {
