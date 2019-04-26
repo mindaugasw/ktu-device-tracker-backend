31b93086c8da0097e418b98ab1e9c190c9072819
Mindaugas <mindaugas932@gmail.com>
2019-04-27 02:24:53 +0300

    Added in-code API documentation.

--- src/Controller/DeviceController.php
+++ src/Controller/DeviceController.php
@@ -10,8 +10,12 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
+use Swagger\Annotations AS SWG;
+use Nelmio\ApiDocBundle\Annotation\Model;
 
-
+/**
+ * @Route("/api")
+ */
 class DeviceController extends AbstractController
 {
 	private $serializer;
@@ -24,7 +28,22 @@ class DeviceController extends AbstractController
 	}
 	
 	/**
-	 * @Route("/device/all", name="device_list")
+	 * @Route("/device/all", name="device_list", methods={"GET"})
+	 * 
+	 * @SWG\Get(
+	 *     summary="Get devices list",
+	 *     description="Gets a list of all devices on the system and their properties.",
+	 * 	   produces={"application/json"},
+	 *     tags={"Devices"},
+	 *     @SWG\Response(
+	 *         response=200,
+	 *         description="Success",
+	 *         @SWG\Schema(
+	 *     			type="array",
+	 *     			@SWG\Items(ref=@Model(type=Device::class))
+	 * 		   )
+	 *     )
+	 * )
 	 */
 	public function getDevicesList()
 	{
@@ -45,7 +64,22 @@ class DeviceController extends AbstractController
 	}
 
 	/**
-	 * @Route("/device/all/history", name="device_all_history")
+	 * @Route("/device/all/history", name="device_all_history", methods={"GET"})
+	 * 
+	 * @SWG\Get(
+	 *     summary="Get all usage history",
+	 *     description="Gets combined usage history for all devices in the system.",
+	 * 	   produces={"application/json"},
+	 *     tags={"Devices"},
+	 *     @SWG\Response(
+	 *         response=200,
+	 *         description="Success",
+	 *         @SWG\Schema(
+	 *     			type="array",
+	 *     			@SWG\Items(ref=@Model(type=UsageEntry::class))
+	 * 		   )
+	 *     )
+	 * )
 	 */
 	public function getAllDevicesHistory()
 	{
@@ -63,7 +97,32 @@ class DeviceController extends AbstractController
 	}
 
 	/**
-	 * @Route("/device/{deviceId}/history", name="device_single_history")
+	 * @Route("/device/{deviceId}/history", name="device_single_history", methods={"GET"})
+	 *
+	 * @SWG\Get(
+	 *     summary="Get device usage history",
+	 *     description="Gets usage history for a single device.",
+	 * 	   produces={"application/json"},
+	 *     tags={"Devices"},
+	 *     @SWG\Parameter(
+	 *         name="deviceId",
+	 *         in="path",
+	 *         description="Device unique identifier (uniqueId).",
+	 *         type="string",
+	 *     ),
+	 *     @SWG\Response(
+	 *         response=200,
+	 *         description="Success",
+	 *         @SWG\Schema(
+	 *     			type="array",
+	 *     			@SWG\Items(ref=@Model(type=UsageEntry::class))
+	 * 		   )
+	 *     ),
+	 *     @SWG\Response(
+	 *         response=404,
+	 *         description="Device not found"
+	 *     )
+	 * )	 
 	 */
 	public function getSingleDeviceHistory(string $deviceId)
 	{
@@ -86,7 +145,52 @@ class DeviceController extends AbstractController
 	}
 	
 	/**
-	 * @Route("device/update", name="device_update")
+	 * @Route("device/update", name="device_update", methods={"POST"})
+	 * 
+	 * @SWG\Post(
+	 *     summary="Update device info",
+	 *     description="Update information about specific device: it's name, SIM card status, or OS.
+				If there is no device with given uniqueId, a new device is created. In that case device name, SIM card status, and OS must all be provided.",
+	 * 	   produces={"application/json"},
+	 *     tags={"Devices"},
+	 *     @SWG\Parameter(
+	 *         name="uniqueId",
+	 *         in="formData",
+	 *         description="Device which to update.",
+	 *         type="string",
+	 *     	   required=true
+	 *     ),
+	 *     @SWG\Parameter(
+	 *         name="name",
+	 *         in="formData",
+	 *         description="Updates device name. If creating new device, uniqueId must be not empty and not whitespace-only.",
+	 *         type="string",
+	 *     ),
+	 *     @SWG\Parameter(
+	 *         name="simCard",
+	 *         in="formData",
+	 *         description="Updates SIM card status.",
+	 *         type="boolean",
+	 *     ),
+	 *     @SWG\Parameter(
+	 *         name="os",
+	 *         in="formData",
+	 *         description="Updates device OS.",
+	 *         type="string",
+	 *     ),
+	 *     @SWG\Response(
+	 *         response=200,
+	 *         description="Success. Returns updated or newly created device.",
+	 *         @SWG\Schema(ref=@Model(type=Device::class)
+	 * 		   )
+	 *     ),
+	 *     @SWG\Response(
+	 *         response=400,
+	 *         description="Bad request. Returns if:
+	               * there's no uniqueId provided
+	               * creating new device and name or OS are not provided"
+	 *     ),
+	 * )
 	 */
 	public function updateDevice(Request $request)
 	{
@@ -133,7 +237,55 @@ class DeviceController extends AbstractController
 	}
 	
 	/**
-	 * @Route("/device/new", name="device_new")
+	 * @Route("/device/new", name="device_new", methods={"POST"})
+	 *
+	 * @SWG\Post(
+	 *     summary="Create new device",
+	 *     description="Creates new device. It's also possible to create new device by sending new uniqueId to /device/new .",
+	 * 	   produces={"application/json"},
+	 *     deprecated=true,
+	 *     tags={"Devices"},
+	 *     @SWG\Parameter(
+	 *         name="deviceId",
+	 *         in="formData",
+	 *         description="Device unique identifier (uniqueId).",
+	 *         type="string",
+	 *     	   required=true
+	 *     ),
+	 *     @SWG\Parameter(
+	 *         name="name",
+	 *         in="formData",
+	 *         description="Device name. Must be not empty and not whitespace-only.",
+	 *         type="string",
+	 *     	   required=true,
+	 *     ),
+	 *     @SWG\Parameter(
+	 *         name="simCard",
+	 *         in="formData",
+	 *         description="SIM card status.",
+	 *         type="boolean",
+	 *     ),
+	 *     @SWG\Parameter(
+	 *         name="os",
+	 *         in="formData",
+	 *         description="Device OS.",
+	 *         type="string",
+	 *         required=true,
+	 *     ),
+	 *     @SWG\Response(
+	 *         response=200,
+	 *         description="Success. Returns newly created device.",
+	 *         @SWG\Schema(ref=@Model(type=Device::class)
+	 * 		   )
+	 *     ),
+	 *     @SWG\Response(
+	 *         response=400,
+	 *         description="Bad request. Returns if:
+	 * uniqueId, name, or OS are not provided
+	 * uniqueId is empty or whitespace-only
+	 * there already exists device with given uniqueId"
+	 *     ),
+	 * )
 	 */
 	public function createNewDevice(Request $request)
 	{
@@ -165,7 +317,44 @@ class DeviceController extends AbstractController
 	}
 	
 	/**
-	 * @Route("/device/delete", name="device_delete")
+	 * @Route("/device/delete", name="device_delete", methods={"POST"})
+	 *
+	 * @SWG\Post(
+	 *     summary="Delete device",
+	 *     description="Deletes device.",
+	 * 	   produces={"application/json"},
+	 *     tags={"Devices"},
+	 *     @SWG\Parameter(
+	 *         name="uniqueId",
+	 *         in="formData",
+	 *         description="Device unique identifier (uniqueId).",
+	 *         type="string",
+	 *     	   required=true
+	 *     ),
+	 *     @SWG\Parameter(
+	 *         name="token",
+	 *         in="formData",
+	 *         description="JWT authorization token. NOT YET IMPLEMENTED.",
+	 *         type="string",
+	 *     	   required=true
+	 *     ),
+	 *     @SWG\Response(
+	 *         response=200,
+	 *         description="Success.",
+	 *     ),
+	 *     @SWG\Response(
+	 *         response=400,
+	 *         description="Bad request. Returns if uniqueId or JWT not provided."
+	 *     ),
+	 *     @SWG\Response(
+	 *         response=401,
+	 *         description="Could not authorize (invalid/expired token)."
+	 *     ),
+	 *     @SWG\Response(
+	 *         response=404,
+	 *         description="Device with given uniqueId not found."
+	 *     ),
+	 * )
 	 */
 	public function deleteDevice(Request $request)
 	{
@@ -192,7 +381,41 @@ class DeviceController extends AbstractController
 	}
 
 	/**
-	 * @Route("/device/register", name="device_register")
+	 * @Route("/device/register", name="device_register", methods={"POST"})
+	 *
+	 * @SWG\Post(
+	 *     summary="Register device",
+	 *     description="Register device to user (to be used when person takes device and scans his QR code with it).",
+	 * 	   produces={"application/json"},
+	 *     tags={"Devices"},
+	 *     @SWG\Parameter(
+	 *         name="uniqueId",
+	 *         in="formData",
+	 *         description="Device unique identifier (uniqueId).",
+	 *         type="string",
+	 *     	   required=true
+	 *     ),
+	 *     @SWG\Parameter(
+	 *         name="userCode",
+	 *         in="formData",
+	 *         description="User's QR code.",
+	 *         type="string",
+	 *     	   required=true
+	 *     ),
+	 *     @SWG\Response(
+	 *         response=200,
+	 *         description="Success.",
+	 *         @SWG\Schema(ref=@Model(type=UsageEntry::class))
+	 *     ),
+	 *     @SWG\Response(
+	 *         response=400,
+	 *         description="Bad request. Returns if uniqueId or userCode are not provided."
+	 *     ),
+	 *     @SWG\Response(
+	 *         response=404,
+	 *         description="Device or user not found."
+	 *     ),
+	 * )
 	 */
 	public function registerDevice(Request $request)
 	{
@@ -231,15 +454,40 @@ class DeviceController extends AbstractController
 	}
 	
 	/**
-	 * Must be at the bottom of the controller as otherwise it also catches all other routes.
-	 *
-	 * @Route("device/{deviceId}", name="device_single")
+	 * @Route("device/{deviceId}", name="device_single", methods={"GET"})
+	 * 
+	 * @SWG\Get(
+	 *     summary="Get single device",
+	 *     description="Gets info about a single device.",
+	 * 	   produces={"application/json"},
+	 *     tags={"Devices"},
+	 *     @SWG\Parameter(
+	 *         name="deviceId",
+	 *         in="path",
+	 *         description="Device unique identifier (uniqueId).",
+	 *         type="string",
+	 *     ),
+	 *     @SWG\Response(
+	 *         response=200,
+	 *         description="Success",
+	 *         @SWG\Schema(ref=@Model(type=Device::class))
+	 *     ),
+	 * 	   @SWG\Response(
+	 *         response=404,
+	 *         description="Device not found",
+	 *     )
+	 * )
+	 * 
+	 * Method nust be at the bottom of the controller as otherwise it also catches all other routes.
 	 */
 	public function getDevice(string $deviceId)
 	{
 		$device = $this->getDoctrine()
 			->getRepository(Device::class)
 			->findOneBy(['uniqueId' => $deviceId]);
+		
+		if (!isset($device))
+			return new Response(null, Response::HTTP_NOT_FOUND, $this->headers);
 		
 		$json = $this->serializer->serialize(
 			$device,
